86af05ab39cff58f83c8f6068da9183d57c9ed52
Raphael R. Lopes <rrlopes07@gmail.com>
2022-05-24 22:36:41 -0300

    Mais um java

---
+++ Java/sintaxe-variaveis-e-fluxo/src/DesafioFatorial.java
@@ -0,0 +1,11 @@
+
+public class DesafioFatorial {
+
+	public static void main(String[] args) {
+		for(int n = 1; n <=10; n++) {
+			System.out.println(fat n);
+		}
+
+	}
+
+}
---
+++ Java/sintaxe-variaveis-e-fluxo/src/TesteMultiplo3.java
@@ -0,0 +1,13 @@
+
+public class TesteMultiplo3 {
+
+	public static void main(String[] args) {
+		for (int n = 0; n <= 100; n++) {
+			if(n % 3 == 0) {
+				System.out.println(n);
+			}
+		}
+
+	}
+
+}
